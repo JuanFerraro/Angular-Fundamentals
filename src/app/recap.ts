d290575d2e46e2f6d3832d35c1c2e2bb0ccf49be
Juan Ferraro <juansebastianferraro@gmail.com>
2023-07-28 18:49:28 -0500

    Create recap.ts, learning basics

---
+++ src/app/recap.ts
@@ -0,0 +1,15 @@
+const username: string = 'JuanBF';
+
+const sum = (a: number, b: number) => {
+  return a + b;
+}
+
+sum(1,3);
+
+class Person {
+
+  constructor(public age:number, public lastName: string){}
+}
+
+const juan = new Person(15, 'Barrios');
+
