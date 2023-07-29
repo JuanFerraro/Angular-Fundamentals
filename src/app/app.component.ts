b252d33ff855ee83cc568913af40f7bc8ece4b2f
Juan Ferraro <juansebastianferraro@gmail.com>
2023-07-29 11:35:18 -0500

    Exploring string interpolation, event binding and property binding 📚

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Juan';
+  age = 26;
+  img = 'https://source.unsplash.com/random'
+  btnDisabled = true;
+
+  person = {
+    name: 'Sebastian',
+    age: 26,
+    avatar: 'https://source.unsplash.com/random',
+  }
+
+  toggleButton(){
+    /* Tomamos el estado y lo negamos */
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increseAge(){
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop)
+  }
+
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
+
+
 }
